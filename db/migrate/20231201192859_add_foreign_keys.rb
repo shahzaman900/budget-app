b11bec6fa1e5a9e2d5361d6a700dc2b9649a25ac
sheezu900 <shahzamanlive2@gmail.com>
2023-12-02 00:59:18 +0500

    complete migrations

--- db/migrate/20231201192859_add_foreign_keys.rb
+++
@@ -1,7 +0,0 @@
-class AddForeignKeys < ActiveRecord::Migration[7.1]
-  def change
-    add_foreign_key :categories_transactions, :categoriestabels, on_delete: :cascade
-    add_foreign_key :categories_transactions, :transactionstabels, on_delete: :cascade
-
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_01_191137) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_01_195618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_01_191137) do
     t.string "surname"
   end
 
-  add_foreign_key "categoriestabels", "users"
-  add_foreign_key "transactionstabels", "users", column: "author_id"
+  add_foreign_key "categories_transactions", "categoriestabels", column: "category_id", on_delete: :cascade
+  add_foreign_key "categories_transactions", "transactionstabels", column: "transaction_id", on_delete: :cascade
+  add_foreign_key "categoriestabels", "users", on_delete: :cascade
+  add_foreign_key "transactionstabels", "users", column: "author_id", on_delete: :cascade
 end
