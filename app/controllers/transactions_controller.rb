ed5c546e9dc8c2f010aa98cc0d19fde2effe3c3b
sheezu900 <shahzamanlive2@gmail.com>
2023-12-02 03:26:30 +0500

    add seeds

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,6 @@
 class TransactionController < ApplicationController
   def index
     @category = Category.find(params[:category_id])
-    @transaction = @category.categoriestabels
+    @transaction = @category.transactionstabels
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+u1 = User.create(name: 'Shah', surname: 'Zaman')
+
+cat1 = Category.create(name: 'School', icon: 'Photo', user: u1)
+cat2 = Category.create(name: 'Rent', icon: 'Photo', user: u1)
+cat3 = Category.create(name: 'shopping', icon: 'Photo', user: u1)
+
+trans1 = Transaction.create(name: 'Lunch', amount: 10, author: u1)
+trans1.categories << cat1
+
+trans2 = Transaction.create(name: 'House rent', amount: 100.50, author: u1)
+trans2.categories << cat2
+
+trans3 = Transaction.create(name: 'Rice', amount: 100, author: u1)
+trans3.categories << cat1
+trans3.categories << cat2
