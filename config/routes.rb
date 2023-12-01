c5e328c7ab8f02b5732279a73e1b3d03dd1c534a
sheezu900 <shahzamanlive2@gmail.com>
2023-12-02 03:09:32 +0500

    add rout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #
+  resources :splash_screen, only: [:index]
+  resources :users do
+    resources :categoriestabels do
+      resources :transactionstabels
+    end
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
