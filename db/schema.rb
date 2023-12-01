21bfa9bad71747dd0787701be5cef98689a83bfe
sheezu900 <shahzamanlive2@gmail.com>
2023-12-01 23:38:21 +0500

    add surname to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_01_121609) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_01_183625) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_01_121609) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "surname"
   end
 
 end
