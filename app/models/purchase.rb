39282ce2746e89fd69122df2a6a0ccaeaaa30e79
sheezu900 <shahzamanlive2@gmail.com>
2023-12-02 19:18:29 +0500

    modify database

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,5 +1,7 @@
-class Purchase < ApplicationRecord
+class purchase < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_and_belongs_to_many :categories
-end
 
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :amount, presence: true, numericality: { money: true, greater_than_or_equal_to: 0 }
+end
--- db/migrate/20231202114355_create_cate_purchase_tbale.rb
+++
@@ -1,9 +0,0 @@
-class CreateCatePurchaseTbale < ActiveRecord::Migration[7.1]
-  def change
-    create_table :category_purchase do |t|
-      t.references :category, foreign_key: { to_table: :categories }
-      t.references :purchase, foreign_key: { to_table: :purchases }
-      t.timestamps
-    end
-  end
-end
