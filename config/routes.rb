edee7b2fc0a687f2fa2316c56db3f1c29431a548
sheezu900 <shahzamanlive2@gmail.com>
2023-12-02 15:34:47 +0500

    .

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   #
   # resources :splash_screen, only: [:index]
   # resources :categories
-  # resources :users do
-  #   resources :categoriestabels do
-  #     resources :transactionstabels
-  #   end
-  # end
+  resources :users do
+    resources :categories do
+      resources :transactions
+    end
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get '/transactions/index'
-  get '/categories/index'
+  # get '/transactions/index'
+  # get '/categories/index'
   root to: 'users#index'
 end
