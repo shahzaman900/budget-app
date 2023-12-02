b68986589cbd7622b10b603b9d6f4bbff555ff18
sheezu900 <shahzamanlive2@gmail.com>
2023-12-02 18:58:46 +0500

    add sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  #
-  resources :splash_screen, only: [:index]
-  resources :users do
-    resources :categories do
-      resources :purchases
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+  }
+  devise_scope :user do
+    authenticated :user do
+      root to: 'categories#index', as: :authenticated_root
     end
+    unauthenticated :user do
+      root to: 'splash_screen#index', as: :unauthenticated_root
+    end
+  end
+  get 'purchases/all_purchases', to: 'purchases#all_purchases', as: 'all_purchases'
+
+  resources :users
+  resources :categories do
+    resources :purchases
   end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -15,7 +24,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  # get '/transactions/index'
-  # get '/categories/index'
-  root to: 'users#index'
 end
